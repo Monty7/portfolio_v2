cf4d785da26ad08c3f487ccf8af86ed2e6f73b0b
Montensia Banks <3188572+Monty7@users.noreply.github.com>
2020-07-15 11:23:35 -0500

    Added background image

--- portfolio_v2/src/App.js
+++ portfolio_v2/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import keyboard from './img/keyboard_2000x1125.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={keyboard} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
